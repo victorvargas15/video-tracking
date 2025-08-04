9351daa4963458c277a7899bb71432667d485c7c
victorvargas15 <victorvargas.contact@gmail.com>
2025-08-04 07:01:04 -0400

    Update video-tracking.js

--- video-tracking.js
+++ video-tracking.js
@@ -3,11 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
   var hasFired = false;
 
   if (video) {
-    video.addEventListener('timeupdate', function () {
-      var percentagePlayed = (video.currentTime / video.duration) * 100;
-
-      if (!hasFired && percentagePlayed >= 50) {
-        fbq('trackCustom', 'Video50Percent', {
+    video.addEventListener('play', function () {
+      if (!hasFired) {
+        fbq('trackCustom', 'VideoPlayed', {
           content_name: 'VSL Main Video',
           content_type: 'video',
           content_ids: ['main-vsl']
